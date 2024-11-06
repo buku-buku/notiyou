8d2e9501fdbff09339979f401b6f113279d91c86
Alang Kim <rlaangh77@naver.com>
2024-11-06 22:14:39 +0900

    implement: 미션 노티 메시지 템플릿에 관한 기능을 제공하는 notification_template_service 작성
    - 미션 노티 메시지 템플릿을 조회하고 수정하는 기능을 제공합니다.

---
+++ lib/services/notification_template_service.dart
@@ -0,0 +1,38 @@
+import '../repositories/notification_template_repository.dart';
+
+class NotificationTemplateService {
+  static Future<String> getSuccessMessageTemplate() async {
+    return await NotificationTemplateRepository.getSuccessMessageTemplate();
+  }
+
+  static Future<String> getFailureMessageTemplate() async {
+    return await NotificationTemplateRepository.getFailureMessageTemplate();
+  }
+
+  static Future<bool> updateSuccessMessageTemplate(String newTemplate) async {
+    if (newTemplate.isEmpty) {
+      return false;
+    }
+    return await NotificationTemplateRepository.setSuccessMessageTemplate(
+        newTemplate);
+  }
+
+  static Future<bool> updateFailureMessageTemplate(String newTemplate) async {
+    if (newTemplate.isEmpty) {
+      return false;
+    }
+    return await NotificationTemplateRepository.setFailureMessageTemplate(
+        newTemplate);
+  }
+
+  static Future<bool> resetMessageTemplates() async {
+    final successReset =
+        await NotificationTemplateRepository.setSuccessMessageTemplate(
+            NotificationTemplateRepository.defaultSuccessMessageTemplate);
+    final failureReset =
+        await NotificationTemplateRepository.setFailureMessageTemplate(
+            NotificationTemplateRepository.defaultFailureMessageTemplate);
+
+    return successReset && failureReset;
+  }
+}
