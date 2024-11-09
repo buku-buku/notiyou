9e95442327353cb24071fc000aea2106327391f9
grighth12 <grighth12@gmail.com>
2024-11-09 19:56:41 +0900

    refactor: try-catch 중첩 삭제, 각 로그인 방식 함수로 분리

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -10,35 +10,48 @@ class LoginPage extends StatelessWidget {
 
   static const String routeName = '/login';
 
+  Future<OAuthToken?> _loginWithKakaoTalk() async {
+    try {
+      return await UserApi.instance.loginWithKakaoTalk();
+    } on PlatformException catch (error) {
+      if (error.code == 'CANCELED') {
+        return null;
+      }
+
+      return await _loginWithKakaoAccount();
+    }
+  }
+
+  Future<OAuthToken?> _loginWithKakaoAccount() async {
+    try {
+      return await UserApi.instance.loginWithKakaoAccount();
+    } catch (error) {
+      return null;
+    }
+  }
+
+  Future<void> _handleKakaoLogin(BuildContext context) async {
+    OAuthToken? token;
+
+    if (await isKakaoTalkInstalled()) {
+      token = await _loginWithKakaoTalk();
+    } else {
+      token = await _loginWithKakaoAccount();
+    }
+
+    if (token != null) {
+      print(token.accessToken);
+      context.go(SignupPage.routeName);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Login')),
       body: Center(
         child: ElevatedButton(
-          onPressed: () async {
-            if (await isKakaoTalkInstalled()) {
-              try {
-                await UserApi.instance.loginWithKakaoTalk();
-              } catch (error) {
-                // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
-                // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
-                if (error is PlatformException && error.code == 'CANCELED') {
-                  return;
-                }
-                // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
-                try {
-                  await UserApi.instance.loginWithKakaoAccount();
-                } catch (error) {}
-              }
-            } else {
-              try {
-                await UserApi.instance.loginWithKakaoAccount();
-              } catch (error) {}
-            }
-
-            context.go(SignupPage.routeName);
-          },
+          onPressed: () => _handleKakaoLogin(context),
           child: const Text('카카오톡으로 시작하기'),
         ),
       ),
