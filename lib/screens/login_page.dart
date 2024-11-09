d9af050c28569294921b0a120dfd18f15c3048b7
grighth12 <grighth12@gmail.com>
2024-11-09 19:38:02 +0900

    rm print

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -20,10 +20,7 @@ class LoginPage extends StatelessWidget {
             if (await isKakaoTalkInstalled()) {
               try {
                 await UserApi.instance.loginWithKakaoTalk();
-                print('카카오톡으로 로그인 성공');
               } catch (error) {
-                print('카카오톡으로 로그인 실패 $error');
-
                 // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
                 // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
                 if (error is PlatformException && error.code == 'CANCELED') {
@@ -32,18 +29,12 @@ class LoginPage extends StatelessWidget {
                 // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
                 try {
                   await UserApi.instance.loginWithKakaoAccount();
-                  print('카카오계정으로 로그인 성공');
-                } catch (error) {
-                  print('카카오계정으로 로그인 실패 $error');
-                }
+                } catch (error) {}
               }
             } else {
               try {
                 await UserApi.instance.loginWithKakaoAccount();
-                print('카카오계정으로 로그인 성공');
-              } catch (error) {
-                print('카카오계정으로 로그인 실패 $error');
-              }
+              } catch (error) {}
             }
 
             context.go(SignupPage.routeName);
