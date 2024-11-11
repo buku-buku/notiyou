63eb8dafa421c14c1bb777579129895da40e4422
Min <the7mincheol@gmail.com>
2024-11-11 11:18:10 +0900

    [NY-27] 설정 페이지는 최초 설정이 아닌 경우에만 하단 내비게이션을 표시한다 (#15)
    * feat: config_page에 최초 진입 여부에 따라 bottom nav 분기 처리
    
    * refactor: 테스트에서 항상 사용할 routes를 별개의 모듈로 분리
    
    * spec: 테스트 파일에서 routes 객체 사용하도록 리팩터
    
    실제 routes 객체를 사용하는 것이 앞으로 작성해야 할 다른 테스트
    파일에서의 routes 설정 관련 중복 코드를 줄이고, 실제 routes 객체를
    사용하는 만큼 더 신뢰도 있는 테스트 코드가 될 수 있어서 리팩터링합니다.
    
    * feat: 회원가입 페이지에서 설정 완료하면 초기 설정 페이지로 이동
    
    초기 설정 페이지란 bottom nav가 보이지 않는 설정 페이지를 의미합니다.
    기술적 관점에서는 route parameter로 `?isOnboarding=true`를 입력해서
    라우팅을 하는 것을 의미합니다.
    
    * [NY-33] BottomNavigation을 사용하는 페이지들은 페이지를 이동하더라도 BottomNavigation은 유지된다 (#17)
    
    * feat: BottomNavigation은 라우트해도  화면에 그대로 유지된다
    
    1. 페이지를 라우팅할 때 BottomNavigation이 있는 페이지에서는
       BottomNavigation은 그대로 유지하고 화면에서 subsection에 대해서만
       라우팅 처리를합니다.
    2. 이러한 목적으로 제공되는 ShellRoute를 사용합니다.
    
    * feat: BottomNav가 있는 경우 없는 경우를 path 로 구분
    
    ShellRoute 바깥(without BottomNav) 에서 처리되는 path와
    ShellRoute 안쪽(with BottomNav)에서 처리되는 path를 구분해야 하므로
    queryParamter로 설정 페이지의 BottomNAv 유무로 결정했던 것을
    path로 결정하도록 변경합니다.
    
    참고로 이러한 변경이 발생하는 원인은 기존에 ShellRoute를 쓰기 전에는
    BottomNav를 보여주는 유무에 대한 제어를 Page 컴포넌트에서 했었지만
    ShellRoute가 도입되면서 BottomNav를 보여주는 유무에 대한 제어를
    ShellRoute가 가져가기 때문입니다.
    
    그리고 ShellRoute로 통제되는 Page와 그렇지 않은 Page를 구분하는 값은
    query parameter로는 처리가 안되고 path 자체가 달라야 하므로 path를
    변경하게 된 것입니다.
    
    * spec: config_page의 라우팅 테스트 제거 및 signup_page 테스트 수정
    
    bottom navigation에 대한 제어를 더 이상 config_page가 하지 않게 됨에
    따라 config_page_test에서 검증하던 bottom navigation 유무 테스트
    케이스를 삭제합니다.
    
    또한 회원 가입 페이지에서 bottom navigation이 없는 config_page로 라우팅 할 때
    query parameter 가 아니라 path로 라우팅하는 것으로 변경됨에 따라
    테스트 코드도 함께 수정합니다.
    
    * refactor: bottonNavigationRoutes 리팩터링
    
    index 숫자 리터럴을 사용하지 않습니다.
    
    bottonNavigation 목록은 설정 객체처럼 따로 분리합니다.

---
+++ lib/routes/index.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import '../screens/home_page.dart';
+import '../screens/login_page.dart';
+import '../screens/signup_page.dart';
+import '../screens/config_page.dart';
+import '../screens/history_page.dart';
+
+final routes = <RouteBase>[
+  GoRoute(
+    path: LoginPage.routeName,
+    builder: (context, state) => const LoginPage(),
+  ),
+  GoRoute(
+    path: SignupPage.routeName,
+    builder: (context, state) => const SignupPage(),
+  ),
+  GoRoute(
+    path: ConfigPage.onboardingRouteName,
+    builder: (context, state) => const ConfigPage(),
+  ),
+  ShellRoute(
+    builder: (context, state, child) {
+      return Scaffold(
+        body: child,
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          currentIndex: _calculateSelectedIndex(context),
+          selectedItemColor: Colors.blue,
+          unselectedItemColor: Colors.grey,
+          showUnselectedLabels: true,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: '홈'),
+            BottomNavigationBarItem(icon: Icon(Icons.history), label: '히스토리'),
+            BottomNavigationBarItem(icon: Icon(Icons.settings), label: '설정'),
+            BottomNavigationBarItem(icon: Icon(Icons.logout), label: '로그아웃'),
+          ],
+          onTap: (index) {
+            context.go(bottomNavigationRoutes[index].path);
+          },
+        ),
+      );
+    },
+    routes: bottomNavigationRoutes,
+  ),
+];
+
+final List<GoRoute> bottomNavigationRoutes = [
+  GoRoute(
+    path: HomePage.routeName,
+    builder: (context, state) => const HomePage(),
+  ),
+  GoRoute(
+    path: HistoryPage.routeName,
+    builder: (context, state) => const HistoryPage(),
+  ),
+  GoRoute(
+    path: ConfigPage.routeName,
+    builder: (context, state) => const ConfigPage(),
+  ),
+];
+
+int _calculateSelectedIndex(BuildContext context) {
+  final String location =
+      GoRouter.of(context).routerDelegate.currentConfiguration.uri.toString();
+  return bottomNavigationRoutes
+      .indexWhere((route) => location.startsWith(route.path));
+}
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:notiyou/models/mission.dart';
-import 'config_page.dart';
-import 'history_page.dart';
-import 'login_page.dart';
 import '../services/mission_service.dart';
 
 class HomePage extends StatefulWidget {
@@ -83,45 +79,6 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
         ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        currentIndex: 0,
-        selectedItemColor: Colors.blue,
-        unselectedItemColor: Colors.grey,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: '홈',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.history),
-            label: '히스토리',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: '설정',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.logout),
-            label: '로그아웃',
-          ),
-        ],
-        onTap: (index) {
-          switch (index) {
-            case 0:
-              break;
-            case 1:
-              context.go(HistoryPage.routeName);
-              break;
-            case 2:
-              context.go(ConfigPage.routeName);
-              break;
-            case 3:
-              context.go(LoginPage.routeName);
-              break;
-          }
-        },
       ),
     );
   }
