92604540c52f378c88a04992e0f63a6c4abacbf3
datalater <the7mincheol@gmail.com>
2024-11-29 21:40:18 +0900

    spec: 불필요한 테스트  의존성을 없애고 간단하게 동작에 대해서만 검증

--- test/screens/signup_page_test.dart
+++ test/screens/signup_page_test.dart
@@ -1,49 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:go_router/go_router.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:notiyou/screens/signup_page.dart';
 import 'package:notiyou/screens/config_page.dart';
-import 'package:notiyou/routes/index.dart';
-import 'package:notiyou/services/mission_service.dart';
-import 'package:notiyou/services/supabase_service.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:mocktail/mocktail.dart';
 
-class MockSupabaseClient extends Mock implements SupabaseClient {}
-
-class MockGotrueClient extends Mock implements GoTrueClient {}
+class MockGoRouter extends Mock implements GoRouter {}
 
 void main() {
-  final MockSupabaseClient mockSupabaseClient = MockSupabaseClient();
-
   group('SignupPage 테스트', () {
-    setUp(() async {
-      SharedPreferences.setMockInitialValues({});
-      SupabaseService.setMockClient(mockSupabaseClient);
-      // TODO: 테스트 전용 Mock 서비스 작성되면 좋겠다.
-      MissionService.switchToLocalRepository();
+    late MockGoRouter mockRouter;
+
+    setUp(() {
+      mockRouter = MockGoRouter();
     });
 
     testWidgets('완료 버튼을 누르면 온보딩 모드로 설정 페이지로 이동한다', (tester) async {
-      final router = GoRouter(
-        initialLocation: SignupPage.routeName,
-        routes: routes,
-      );
+      when(() => mockRouter.go(any())).thenReturn(null);
 
       await tester.pumpWidget(
-        MaterialApp.router(
-          routerConfig: router,
+        MaterialApp(
+          home: InheritedGoRouter(
+            goRouter: mockRouter,
+            child: const SignupPage(),
+          ),
         ),
       );
 
       await tester.tap(find.text('완료'));
       await tester.pumpAndSettle();
 
-      expect(
-        router.routeInformationProvider.value.uri.toString(),
-        ConfigPage.onboardingRouteName,
-      );
+      verify(() => mockRouter.go(ConfigPage.onboardingRouteName)).called(1);
     });
   });
 }
