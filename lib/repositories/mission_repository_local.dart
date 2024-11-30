9bb18c1e8693739c0baa392589606ca3d9a83029
Alang Kim <rlaangh77@gmail.com>
2024-11-30 15:58:58 +0900

    refactor: mission_repository_local 불필요 메서드 제거 및 override 노테이션 추가

--- lib/repositories/mission_repository_local.dart
+++ lib/repositories/mission_repository_local.dart
@@ -16,7 +16,6 @@ import '../utils/time_utils.dart';
 ///
 /// ⚠️: 로컬의 데이터는 오늘의 데이터만 저장됩니다.
 /// 오늘 이후의 모든 데이터는 앱 실행 시 삭제됩니다.
-
 class MissionRepositoryLocal implements MissionRepository {
   SharedPreferences? _prefs;
   final String _missionStoreKey = 'mission';
@@ -35,43 +34,17 @@ class MissionRepositoryLocal implements MissionRepository {
   // private 생성자
   MissionRepositoryLocal._internal();
 
+  @override
   Future<void> init() async {
     await _missionTimeRepository.init();
     _prefs ??= await SharedPreferences.getInstance();
   }
 
-  // 미션 별 키 생성
-  String _getMissionKey(int missionNumber) => 'mission$missionNumber';
-
-  // 날짜별 키 생성.
   String _getKeyForDate(DateTime date) {
     return '${_missionStoreKey}_${TimeUtils.stringifyYearMonthDay(date)}';
   }
 
-  /// Shared Preferences에 저장되어 있는 날짜별 키 목록
-  /// ```dart
-  /// final keys = MissionRepository._getDateKeys();
-  /// print(keys); // ['mission_2024-11-04', 'mission_2024-11-03', ...]
-  /// ```
-  List<String> _getDateKeys() {
-    return _prefs!
-        .getKeys()
-        .where((key) =>
-            TimeUtils.isDateString(key) && key.startsWith(_missionStoreKey))
-        .toList();
-  }
-
-  /// 날짜 키로부터 날짜 값 파싱
-  /// ```dart
-  /// final date = MissionRepository._parseDateFromKey('mission_2024-11-04');
-  /// print(date); // DateTime(2024, 11, 4)
-  /// ```
-  DateTime _parseDateFromKey(String key) {
-    return TimeUtils.parseDate(key.replaceFirst('${_missionStoreKey}_', ''));
-  }
-
   /// 미션 데이터 저장
-  /// TODO: 실제로 데이터가 생성되는 것은 서버에서 수행될 예정입니다.
   /// 이후 해당 메서드는 서버에서 데이터를 받아오는 것을 대비한 캐싱 목적으로만 존재합니다.
   Future<void> _setMissions(DateTime date, List<Mission> missions) async {
     final key = _getKeyForDate(date);
@@ -82,12 +55,7 @@ class MissionRepositoryLocal implements MissionRepository {
     await _prefs!.setStringList(key, missionJsonList);
   }
 
-  Future<void> _addMission(DateTime date, Mission mission) async {
-    final missions = await findMissions(date);
-    final updatedMissions = [...missions, mission];
-    await _setMissions(date, updatedMissions);
-  }
-
+  @override
   Future<void> updateTodayMissionTime(
     int missionNumber,
     TimeOfDay time,
@@ -95,60 +63,25 @@ class MissionRepositoryLocal implements MissionRepository {
     throw UnimplementedError('updateTodayMissionTime is not implemented');
   }
 
-  // 미션 데이터 수정
+  @override
   Future<void> updateMission(Mission mission) async {
     throw UnimplementedError('updateMission is not implemented');
   }
 
-  /// ? 이것까지 레포지토리에 위치시키는게 좋을지 고민이 되긴함..
-  Future<List<Mission>> _createTodaysMissions(DateTime date) async {
-    final [mission1Time, mission2Time] = await Future.wait([
-      _missionTimeRepository.getMissionTime(1),
-      _missionTimeRepository.getMissionTime(2),
-    ]);
-
-    return [
-      if (mission1Time != null)
-        Mission(
-          id: _getMissionKey(1),
-          missionNumber: 1,
-          time: mission1Time,
-          isCompleted: false,
-          date: date,
-        ),
-      if (mission2Time != null)
-        Mission(
-          id: _getMissionKey(2),
-          missionNumber: 2,
-          time: mission2Time,
-          isCompleted: false,
-          date: date,
-        ),
-    ];
-  }
-
-  // 미션 데이터 불러오기
-  Future<List<Mission>> findMissions(DateTime date,
-      {bool createIfEmpty = false}) async {
+  @override
+  Future<List<Mission>> findMissions(DateTime date) async {
     final key = _getKeyForDate(date);
     if (_prefs == null) await init();
 
     final missionJsonList = _prefs!.getStringList(key) ?? [];
-
-    // 미션 데이터가 없으면 로컬에서 임의로 생성한다.
-    if (missionJsonList.isEmpty && createIfEmpty) {
-      final newMissions = await _createTodaysMissions(date);
-
-      await _setMissions(date, newMissions);
-      return newMissions;
-    }
 
     return missionJsonList
         .map((json) => Mission.fromJson(jsonDecode(json)))
         .toList();
   }
 
-  /// 현재는 오늘의 미션에 대한 찾기만 지원함.
+  /// ! 현재는 오늘의 미션에 대한 찾기만 지원함.
+  @override
   Future<Mission?> findMissionById(String id) async {
     final missions = await findMissions(DateTime.now());
     try {
@@ -158,7 +91,6 @@ class MissionRepositoryLocal implements MissionRepository {
     }
   }
 
-  // 미션 번호로 미션 찾기
   Future<Mission?> _findMissionByMissionNumber(
       DateTime date, int missionNumber) async {
     final missions = await findMissions(date);
