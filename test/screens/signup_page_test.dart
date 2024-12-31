2454567bd250d6df7a629d7dada1c375969687b1
Alang Kim <rlaangh77@gmail.com>
2024-12-31 19:32:34 +0900

    test: sign_up 페이지 테스트 케이스 수정

--- test/screens/signup_page_test.dart
+++ test/screens/signup_page_test.dart
@@ -4,6 +4,7 @@ import 'package:go_router/go_router.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:notiyou/screens/signup_page.dart';
 import 'package:notiyou/screens/challenger_config_page.dart';
+import 'package:notiyou/screens/supporter_signup_page.dart';
 
 class MockGoRouter extends Mock implements GoRouter {}
 
@@ -15,7 +16,7 @@ void main() {
       mockRouter = MockGoRouter();
     });
 
-    testWidgets('완료 버튼을 누르면 온보딩 모드로 설정 페이지로 이동한다', (tester) async {
+    testWidgets('도전자 회원가입 버튼을 누르면 온보딩 모드로 설정 페이지로 이동한다', (tester) async {
       when(() => mockRouter.go(any())).thenReturn(null);
 
       await tester.pumpWidget(
@@ -27,11 +28,29 @@ void main() {
         ),
       );
 
-      await tester.tap(find.text('완료'));
+      await tester.tap(find.text('도전자'));
       await tester.pumpAndSettle();
 
       verify(() => mockRouter.go(ChallengerConfigPage.onboardingRouteName))
           .called(1);
+    });
+
+    testWidgets('조력자 회원가입 버튼을 누르면 조력자 회원가입 페이지로 이동한다', (tester) async {
+      when(() => mockRouter.go(any())).thenReturn(null);
+
+      await tester.pumpWidget(
+        MaterialApp(
+          home: InheritedGoRouter(
+            goRouter: mockRouter,
+            child: const SignupPage(),
+          ),
+        ),
+      );
+
+      await tester.tap(find.text('조력자'));
+      await tester.pumpAndSettle();
+
+      verify(() => mockRouter.go(SupporterSignupPage.routeName)).called(1);
     });
   });
 }
