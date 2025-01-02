87d3668bd440a14637f6f7bedde9e46676038069
Alang Kim <rlaangh77@gmail.com>
2025-01-03 00:38:35 +0900

    feat: 도전자 코드 생성 및 복호화 기능의 challenger_code_service 추가

--- lib/services/challenger_code/challenger_code_exception.dart
+++ lib/services/challenger_code/challenger_code_exception.dart
@@ -16,7 +16,9 @@ enum ChallengerCodeExceptionType {
   invalid('유효하지 않은 코드입니다'),
   notFound('존재하지 않는 도전자 코드입니다'),
   expired('만료된 코드입니다'),
-  alreadyUsed('이미 사용된 코드입니다');
+  alreadyUsed('이미 사용된 코드입니다'),
+  creationFailed('코드 생성에 실패했습니다'),
+  unknown('알 수 없는 오류');
 
   final String message;
   const ChallengerCodeExceptionType(this.message);
