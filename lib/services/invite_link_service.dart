eda46ed6605a70c87106ca86cadd7048434157bd
42KIM <ksahoo524@gmail.com>
2025-01-12 16:31:14 +0900

    refactor: depreacted된 uni_links를 app_links로 교체

--- lib/services/invite_link_service.dart
+++ lib/services/invite_link_service.dart
@@ -1,22 +1,22 @@
+import 'package:app_links/app_links.dart';
 import 'package:notiyou/routes/router.dart';
 import 'package:notiyou/screens/login_page.dart';
-import 'package:uni_links/uni_links.dart';
 
 class InviteLinkService {
+  static final _appLinks = AppLinks();
+
   static Future<void> init() async {
     try {
       // 앱이 종료된 상태에서 링크로 열린 경우
-      final initialUri = await getInitialUri();
+      final initialUri = await _appLinks.getInitialAppLink();
       if (initialUri != null) {
         _handleLink(initialUri);
       }
 
       // 앱이 실행 중일 때 링크를 처리
-      uriLinkStream.listen(
-        (Uri? uri) {
-          if (uri != null) {
-            _handleLink(uri);
-          }
+      _appLinks.uriLinkStream.listen(
+        (uri) {
+          _handleLink(uri);
         },
         onError: (error) {
           _handleError('초대링크 스트림 에러', error);
@@ -30,9 +30,8 @@ class InviteLinkService {
   static void _handleLink(Uri uri) {
     try {
       if (uri.path.startsWith('/invite/')) {
-        final challengerInviteCode = uri.pathSegments.last;
-        router.push(LoginPage.routeName,
-            extra: {'challengerCode': challengerInviteCode});
+        final userId = uri.pathSegments.last;
+        router.push(LoginPage.routeName, extra: {'challengerCode': userId});
       } else {
         throw Exception('올바른 초대링크가 아닙니다');
       }
@@ -42,11 +41,13 @@ class InviteLinkService {
   }
 
   static void _handleError(String message, dynamic error) {
-    // TODO: 에러 로깅
+    // 에러 로깅
+    print('InviteLinkService 에러: $message - $error');
+
     try {
       router.go(LoginPage.routeName); // 또는 다른 적절한 fallback 라우트
     } catch (error) {
-      throw Exception('리다이렉트 실패: $error');
+      print('리다이렉트 실패: $error');
     }
   }
 }
