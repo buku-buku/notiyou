42bdc6a10fcd0dcd737b9af8e0e10745728aaded
Alang Kim <rlaangh77@gmail.com>
2025-03-25 01:21:58 +0900

    feat: 기존 router에 authGuard, roleGuard 적용

--- lib/routes/index.dart
+++ lib/routes/index.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:notiyou/core/routes/guards/route_guard.dart';
+import 'package:notiyou/core/routes/route/go_route_with_guards.dart';
 import 'package:notiyou/models/registration_status.dart';
-import 'package:notiyou/screens/supporter_config_page.dart';
-import 'package:notiyou/services/auth/auth_service.dart';
+import 'package:notiyou/routes/guards/auth_guard.dart';
+import 'package:notiyou/routes/guards/role_guard.dart';
+import 'package:notiyou/screens/challenger_config_page.dart';
+import 'package:notiyou/screens/history_page.dart';
 import 'package:notiyou/screens/home_page.dart';
 import 'package:notiyou/screens/login_page.dart';
 import 'package:notiyou/screens/signup_page.dart';
-import 'package:notiyou/screens/challenger_config_page.dart';
-import 'package:notiyou/screens/history_page.dart';
 import 'package:notiyou/screens/splash_page.dart';
+import 'package:notiyou/screens/supporter_config_page.dart';
 import 'package:notiyou/screens/supporter_signup_page.dart';
+import 'package:notiyou/services/auth/auth_service.dart';
 
 final routes = <RouteBase>[
   GoRoute(
@@ -22,21 +26,36 @@ final routes = <RouteBase>[
       initialChallengerCode: state.uri.queryParameters['challengerId'],
     ),
   ),
-  GoRoute(
+  GoRouteWithGuards(
     path: SignupPage.routeName,
     builder: (context, state) => const SignupPage(),
+    guards: [
+      applyGuards(decorators: [
+        (guard) => AuthGuard(guard),
+      ])
+    ],
   ),
-  GoRoute(
+  GoRouteWithGuards(
     path: SupporterSignupPage.routeName,
     builder: (context, state) => SupporterSignupPage(
       initialChallengerCode: state.uri.queryParameters['challengerId'],
     ),
+    guards: [
+      applyGuards(decorators: [
+        (guard) => AuthGuard(guard),
+      ])
+    ],
   ),
-  GoRoute(
+  GoRouteWithGuards(
     path: ChallengerConfigPage.onboardingRouteName,
     builder: (context, state) => const ChallengerConfigPage(
       isFirstTime: true,
     ),
+    guards: [
+      applyGuards(decorators: [
+        (guard) => AuthGuard(guard),
+      ])
+    ],
   ),
   ShellRoute(
     builder: (context, state, child) {
@@ -83,21 +102,45 @@ final routes = <RouteBase>[
 ];
 
 final List<GoRoute> bottomNavigationRoutes = [
-  GoRoute(
+  GoRouteWithGuards(
     path: HomePage.routeName,
     builder: (context, state) => const HomePage(),
+    guards: [
+      applyGuards(decorators: [
+        (guard) => AuthGuard(guard),
+        (guard) => RoleGuard(guard, [UserRole.challenger, UserRole.supporter]),
+      ])
+    ],
   ),
-  GoRoute(
+  GoRouteWithGuards(
     path: HistoryPage.routeName,
     builder: (context, state) => const HistoryPage(),
+    guards: [
+      applyGuards(decorators: [
+        (guard) => AuthGuard(guard),
+        (guard) => RoleGuard(guard, [UserRole.challenger, UserRole.supporter]),
+      ])
+    ],
   ),
-  GoRoute(
+  GoRouteWithGuards(
     path: ChallengerConfigPage.routeName,
     builder: (context, state) => const ChallengerConfigPage(),
+    guards: [
+      applyGuards(decorators: [
+        (guard) => AuthGuard(guard),
+        (guard) => RoleGuard(guard, [UserRole.challenger]),
+      ])
+    ],
   ),
-  GoRoute(
+  GoRouteWithGuards(
     path: SupporterConfigPage.routeName,
     builder: (context, state) => const SupporterConfigPage(),
+    guards: [
+      applyGuards(decorators: [
+        (guard) => AuthGuard(guard),
+        (guard) => RoleGuard(guard, [UserRole.supporter]),
+      ])
+    ],
   ),
 ];
 
--- lib/routes/router.dart
+++ lib/routes/router.dart
@@ -1,11 +1,12 @@
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:notiyou/core/routes/route/go_route_with_guards.dart';
 import 'package:notiyou/routes/index.dart';
 import 'package:notiyou/screens/home_page.dart';
 import 'package:notiyou/screens/login_page.dart';
 import 'package:notiyou/screens/splash_page.dart';
 import 'package:notiyou/screens/supporter_signup_page.dart';
 import 'package:notiyou/services/invite_deep_link_service.dart';
-import 'package:flutter/foundation.dart';
 
 final routerRefreshNotifier = ValueNotifier<bool>(false);
 
@@ -21,10 +22,31 @@ final router = GoRouter(
       return routeName;
     }
 
+    final guardRedirectPath =
+        await _checkGuardRedirects(state.topRoute, context, state);
+    if (guardRedirectPath != null) {
+      return guardRedirectPath;
+    }
+
     return null;
   },
   routes: routes,
 );
+
+Future<String?> _checkGuardRedirects(
+    GoRoute? route, BuildContext context, GoRouterState state) async {
+  if (route is GoRouteWithGuards) {
+    final guards = route.guards;
+    for (final guard in guards) {
+      final canActivate = await guard.canActivate(context, state, null);
+      if (canActivate != null) {
+        return canActivate;
+      }
+    }
+  }
+
+  return null;
+}
 
 String? _getRouteFromInviteDeepLink(InviteDeepLinkInfo deepLink) {
   return switch (deepLink.userStatus) {
